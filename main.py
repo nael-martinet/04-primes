7ba0936cb8fa21b9537f2dd295aff4e457fb6433
nael-martinet <nael.martinet@edu.esiee.fr>
2025-11-06 09:51:13 +0000

    fichier contenant un ensemble de fonction permettant de tester la primalité d'un nombre

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
+"""fonction racine carrée de math"""
 from math import sqrt
 
 #### Fonction secondaire
 
-
-def isprime(p):
-
-    # votre code ici
-
-    pass
-
+def isprime(p: int) -> bool:
+    """fonction premettant de retourner si le param p est premier ou non.
+    Args:
+        p (int): nombre à tester.
+    Returns:
+        bool: True si premier, False sinon.
+    """
+    if p<2:
+        return False
+    for i in range(2,round(sqrt(p)+1)):
+        if p % i == 0:
+            return False
+    return True
 #### Fonction principale
 
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """
+    La fonction main
+    """
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
